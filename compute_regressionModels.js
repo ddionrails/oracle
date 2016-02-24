9630ad1a25f69e7b585a60ef19d5d256617b843c
Carolin Stolpe <cstolpe@diw.de>
2016-02-24 19:07:06 +0100

    Add bubble chart

--- compute_regressionModels.js
+++ compute_regressionModels.js
@@ -3,33 +3,25 @@
 function computeRegressionModels(userInput, values){
 	
 	var output = {};
-	var abc  = [];
 
 	for (key in values){
-		var output = 0;
+		var tmp = 0;
 		for (property in values[key].calc){
 		
 			if(userInput[property].constructor === Array){
 				for(i=0; i < userInput[property].length; i++){
-					output += userInput[property][i] * values[key].calc[property];
+					tmp += userInput[property][i] * values[key].calc[property];
 				};
 				continue;
 			};
 			
-			output += userInput[property] * values[key].calc[property];	
+			tmp += userInput[property] * values[key].calc[property];	
 		};
-		output[key] = output;
-		console.log(values[key] + "" + output);
+		output[key] = tmp;
+		console.log(values[key] + "" + tmp);
 	};
+	
+	return output;
 	
 };
 	
-
-
-/**
-for satisfaction
-	for letter
-		output + = userInput[letter] * values[satisfaction][letter];   
-	for sex
-	for coefficient				
-**/
